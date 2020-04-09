35e417bd7f8ba7afa4660c8f9b680a12a478cb3c
theapache64 <theapache64@gmail.com>
2020-04-10 00:07:15 +0530

    ◀️ set db connection : backup

--- telegram-bot/src/main/kotlin/com/theapache64/flare/telegrambot/OnCommandServlet.kt
+++ telegram-bot/src/main/kotlin/com/theapache64/flare/telegrambot/OnCommandServlet.kt
@@ -1,3 +1,5 @@
+package com.theapache64.flare.telegrambot
+
 import javax.servlet.annotation.WebServlet
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
@@ -6,6 +8,6 @@ import javax.servlet.http.HttpServletResponse
 @WebServlet(urlPatterns = ["/on_command"])
 class OnCommandServlet : HttpServlet() {
     override fun doPost(req: HttpServletRequest?, resp: HttpServletResponse?) {
-        println("hit!")
+        println("hit! ${Connection.connection}")
     }
 }
---
+++ telegram-bot/src/main/kotlin/com/theapache64/flare/telegrambot/Users.kt
@@ -0,0 +1,5 @@
+package com.theapache64.flare.telegrambot
+
+object Users {
+
+}
