cfaed68a615c04e68190076be0be0a0a471b5ab0
theapache64 <theapache64@gmail.com>
2020-04-09 16:48:03 +0530

    🌄 init command servlet

---
+++ telegram-bot/src/main/kotlin/OnCommandServlet.kt
@@ -0,0 +1,11 @@
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@WebServlet(urlPatterns = ["/on_command"])
+class OnCommandServlet : HttpServlet() {
+    override fun doPost(req: HttpServletRequest?, resp: HttpServletResponse?) {
+        println("hit!")
+    }
+}
